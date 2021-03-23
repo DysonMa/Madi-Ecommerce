b0c83d66690e78c23b60d6aec27f01b0d56d4071
Dyson <madihsiang@gmail.com>
2021-03-23 09:34:11 +0800

    'heroku'

--- Ecommerce/wsgi.py
+++ Ecommerce/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling  #for Heroku
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application()) #for Heroku
